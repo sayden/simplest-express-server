9a62565e46a7d3b0c05d7e0f37fcd6075e3d7c3f
Mario Castro <mariocaster@gmail.com>
2015-12-06 13:24:16 +0100

    Watch enabled

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Ansible from PM2!!!');
+  res.send('Hello World Ansible from PM2 and watch enabled!!!');
 });
 
 var server = app.listen(3000, function () {
