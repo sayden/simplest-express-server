b7b6940ddc2b6d0a9662d28830be95eb90f82f97
Mario Castro <mariocaster@gmail.com>
2015-12-08 13:55:40 +0100

    Jenkins!

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Ansible from Docker, CD and the new docker_image plugin!!!');
+  res.send('Hello World Ansible from Docker, CD, the new docker_image plugin and Jenkins!!!');
 });
 
 var server = app.listen(3000, function () {
