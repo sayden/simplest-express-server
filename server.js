143c09b2ca1aa8dea6ed5b8f54cf8e9342771cef
Mario Castro <mariocaster@gmail.com>
2015-12-08 12:59:00 +0100

    CD added to the message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Ansible from Docker!!!');
+  res.send('Hello World Ansible from Docker and CD!!!');
 });
 
 var server = app.listen(3000, function () {
