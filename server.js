674a750be17551a54b4f3b33bfe2bcf3b8a3cfcc
Mario Castro <mariocaster@gmail.com>
2015-12-08 13:11:05 +0100

    Moreeee

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Ansible from Docker and CD!!!');
+  res.send('Hello World Ansible from Docker, CD and the new docker_image plugin!!!');
 });
 
 var server = app.listen(3000, function () {
