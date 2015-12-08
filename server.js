3272866ce3a3c8c424b6491d452a1813c2e69e67
Mario Castro <mariocaster@gmail.com>
2015-12-08 12:48:03 +0100

    Docker tests

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Ansible from PM2 and watch enabled!!!');
+  res.send('Hello World Ansible from Docker!!!');
 });
 
 var server = app.listen(3000, function () {
