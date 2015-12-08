8e904d8668e4d998a9f56a44bc7eb10e2f07ff97
Mario Castro <mariocaster@gmail.com>
2015-12-08 14:02:52 +0100

    Git polling on jenkins

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Ansible from Docker, CD, the new docker_image plugin and Jenkins!!!');
+  res.send('Hello World Ansible from Docker, CD, the new docker_image plugin, Jenkins aaaand git polling!!!');
 });
 
 var server = app.listen(3000, function () {
